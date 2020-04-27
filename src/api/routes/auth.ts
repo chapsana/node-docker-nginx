a230868acd7063b1ff5a04878ffd6eb330e7f042
Alpha Olomi <alphaolomi@gmail.com>
2020-04-27 15:47:13 +0300

    feat(docs): api docs :scroll:

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -6,28 +6,100 @@ import { controllerContainer } from '../../containers/controller.container';
 
 export const router: Router = Router();
 
-// @METHOD: GET
-// @PATH: /api/auth/
-// @DESC: auth route health
-// @ACCESS: public
+/**
+ * @api {get} /api/v1/auth/ Auth Check Health
+ * @apiName GetAuthHealth
+ * @apiVersion 1.0.0
+ * @apiGroup Guest
+ * @apiPermission none
+ * 
+ * @apiExample Example usage:
+ * curl -i http://localhost:3000/api/v1/auth/
+ *
+ * @apiSuccess {String} message Health Status.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "Auth working!",
+ *     }
+ */
 router.get('/', (req: Request, res: Response) => {
     res.json('Auth working!')
 });
 
-// @METHOD: POST
-// @PATH: /api/auth/register
-// @DESC: create new user to database
-// @ACCESS: public
+/**
+ * @api {post} /api/v1/auth/register Create a new User
+ * @apiVersion 1.0.0
+ * @apiName PostUser
+ * @apiGroup Guest
+ * @apiPermission none
+ *
+ * @apiExample Example usage:
+ * curl -i http://localhost:3000/api/v1/auth/register
+ * 
+ * @apiDescription In this case "apiUse" is defined and used.
+ * Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ * @apiParam {String} name      Name of the User.
+ * @apiParam {String} email     Email of the User.
+ * @apiParam {String} password  Password of the User.
+ * 
+ * @apiSuccess {String} id         The new User-ID.
+ *
+ * @apiUse CreateUserError
+ */
 router.post('/register', Validators.getSignUpValidators(), controllerContainer(signUp));
 
-// @METHOD: POST
-// @PATH: /api/auth/login
-// @DESC: login the user
-// @ACCESS: public
+
+/**
+ * @api {post} /api/v1/auth/login Login a User
+ * @apiVersion 1.0.0
+ * @apiName LoginUser
+ * @apiGroup Guest
+ * @apiPermission none
+ * 
+ * @apiExample Example usage:
+ * curl -i http://localhost:3000/api/v1/auth/login
+ * 
+ * @apiDescription In this case "apiUse" is defined and used.
+ * Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ * @apiParam {String} email     Email of the User.
+ * @apiParam {String} password  Password of the User.
+ *
+ * @apiSuccess {String} id         The new User-ID.
+ *
+ * @apiUse CreateUserError
+ */
 router.post('/login', Validators.getLoginValidators(), controllerContainer(login));
 
-// @METHOD: GET
-// @PATH: /api/auth/me
-// @DESC: fetching the user profile
-// @ACCESS: private
+
+/**
+ * @api {get} /api/v1/auth/me Get user profile
+ * @apiVersion 1.0.0
+ * @apiName GetUserProfile
+ * @apiGroup User
+ * @apiPermission user
+ *
+ * @apiExample Example usage:
+ * curl -i http://localhost/api/v1/auth/me
+ *
+ * @apiSuccess {String}   id            The Users-ID.
+ * @apiSuccess {String}   name          Fullname of the User.
+ * @apiSuccess {String}   email         Email of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
 router.get('/me', isAuthenticated, getProfile);
